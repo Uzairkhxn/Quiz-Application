ca51e6b683995f0661af0e55d58910b80891005f
Uzairkhxn <uzairkhan2005r@gmail.com>
2024-05-21 17:27:08 +0000

    updated

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,7 +7,7 @@ const testimonials = [
         content:
             'This quiz web app is built using React JS. It can allow the user to select the criteria for their quiz from a list of options. The user can then proceed to answer the questions in the quiz and their performance will be tracked and shown in the scoreboard. Moreover, the app can be designed to be responsive and user-friendly.',
         avatar:
-            'assets/admin.png',
+            'Assets/admin.png',
     }
 ];
 
